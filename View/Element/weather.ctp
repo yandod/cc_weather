3a439059888f9a36a77a5b62b63ce5bc75461317
hamaco <hamanaka.kazuhiro@gmail.com>
2012-06-29 20:24:01 +0900

    天気を表示できる地域を選べるようにした

--- View/Element/weather.ctp
+++ View/Element/weather.ctp
@@ -1,1 +1,5 @@
-<script language="javascript" charset="euc-jp" type="text/javascript" src="http://weather.livedoor.com/plugin/common/forecast/27.js"></script>
+<?php
+$prefecture_id =(int) $currentuser['UserPreference']['pref']['prefecture'];
+?>
+
+<script language="javascript" charset="euc-jp" type="text/javascript" src="http://weather.livedoor.com/plugin/common/forecast/<?php echo $prefecture_id ?>.js"></script>
--- init.php
+++ init.php
@@ -1,11 +1,18 @@
 <?php
+
 $hookContainer = ClassRegistry::getObject('HookContainer');
 $hookContainer->registerElementHook(
-	'news', // target element name.
-	'../../Plugin/CcWeather/View/Element/weather', // additional template you want to inject.
-	true // it should be true when you want to inject before the target template.
+	'news',
+	'../../Plugin/CcWeather/View/Element/weather',
+	true
 );
+
+$hookContainer->registerElementHook(
+  'my/preferences',
+  '../../Plugin/CcWeather/View/Element/weather_preferences',
+  false
+);
+
 $pluginContainer = ClassRegistry::getObject('PluginContainer');
 $pluginContainer->installed('cc_weather','0.1');
-
 
